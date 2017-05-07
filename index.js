171430d4c124e451dd3e43f78b979c7635f1f0fe
lg <lg.owns@gmail.com>
2017-05-07 22:22:08 +0300

    fix

--- index.js
+++ index.js
@@ -11,19 +11,27 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/generateNewBoard', function (req, res) {
 	const puzzleSolver = new LocalSolver();
-	puzzleSolver.generateNewBoard().then(data => res.json(data));
+	
+	puzzleSolver
+		.generateNewBoard()
+		.then(data => res.json(data));
 })
 
 app.get('/getRandomMoves', function (req, res) {
 	const puzzleSolver = new LocalSolver();
-	const data = puzzleSolver.getRandomMoves(req.query.emptyTilePosition, req.query.moves);
-	res.json(data)
+	
+	puzzleSolver
+		.getRandomMoves(req.query.emptyTilePosition, req.query.moves)
+		.then(data => res.json(data));
 })
 
 app.get('/solve', function (req, res) {
 	console.log('solve', req.query)
 	const puzzleSolver = new LocalSolver();
-	puzzleSolver.solve(JSON.parse('['+req.query.board+']')).then(data => res.json(data));
+	
+	puzzleSolver
+		.solve(JSON.parse('['+req.query.board+']'))
+		.then(data => res.json(data));
 })
 
 const PORT = process.env.PORT || 3000;
